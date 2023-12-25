0dc4aad8f65b7610e614d13be20e5a1bad20ca3a
PREMKUMAR5519 <premkumarananthan1@gmail.com>
2023-12-25 15:29:24 +0530

    first commit

---
+++ 1/task and title.js
@@ -0,0 +1,159 @@
+day1> db.day1.find({MONTH:"october"},{MONTH:1,TITLE:1,CONTENT:1,ACTIVITIES:1,ID:1,_id:0})
+[
+  {
+    ID: 8,
+    TITLE: 'NO_CLASSS',
+    ACTIVITIES: 'NO_CLASSS',
+    CONTENT: 'NO_CLASSS',
+    MONTH: 'october'
+  },
+  {
+    ID: 9,
+    TITLE: 'Javascript',
+    ACTIVITIES: 'details of person in class ',
+    CONTENT: 'OOP in JS',
+    MONTH: 'october'
+  },
+  {
+    ID: 10,
+    TITLE: 'Javascript',
+    ACTIVITIES: 'restcountries API data',
+    CONTENT: 'Array methods',
+    MONTH: 'october'
+  },
+  {
+    ID: 11,
+    TITLE: 'Javascript',
+    ACTIVITIES: 'NO-TASK',
+    CONTENT: 'Reduce & Recap',
+    MONTH: 'october'
+  },
+  {
+    ID: 12,
+    TITLE: 'HTML',
+    ACTIVITIES: 'NO-TASK',
+    CONTENT: 'HTML intro',
+    MONTH: 'october'
+  },
+  {
+    ID: 13,
+    TITLE: 'HTML & CSS',
+    ACTIVITIES: 'NO-TASK',
+    CONTENT: 'HTML & CSS',
+    MONTH: 'october'
+  },
+  {
+    ID: 14,
+    TITLE: 'NO_CLASSS',
+    ACTIVITIES: 'NO_CLASSS',
+    CONTENT: 'NO_CLASSS',
+    MONTH: 'october'
+  },
+  {
+    ID: 15,
+    TITLE: 'HTML & CSS',
+    ACTIVITIES: 'NO-TASK',
+    CONTENT: 'grid,flex,position',
+    MONTH: 'october'
+  },
+  {
+    ID: 16,
+    TITLE: 'HTML & CSS',
+    ACTIVITIES: 'NO-TASK',
+    CONTENT: 'measurements,grid,flex',
+    MONTH: 'october'
+  },
+  {
+    ID: 17,
+    TITLE: 'NO_CLASSS',
+    ACTIVITIES: 'NO_CLASSS',
+    CONTENT: 'NO_CLASSS',
+    MONTH: 'october'
+  },
+  {
+    ID: 18,
+    TITLE: 'HTML & CSS',
+    ACTIVITIES: 'NO-TASK',
+    CONTENT: 'fonts,icon,media query',
+    MONTH: 'october'
+  },
+  {
+    ID: 19,
+    TITLE: 'HTML & CSS',
+    ACTIVITIES: 'interface design ',
+    CONTENT: 'bootstrap',
+    MONTH: 'october'
+  },
+  {
+    ID: 20,
+    TITLE: 'HTML & CSS',
+    ACTIVITIES: 'NO-TASK',
+    CONTENT: 'bootstrap',
+    MONTH: 'october'
+  },
+  {
+    ID: 21,
+    TITLE: 'DOM',
+    ACTIVITIES: 'NO-TASK',
+    CONTENT: 'intro to DOM',
+    MONTH: 'october'
+  },
+  {
+    ID: 22,
+    TITLE: 'DOM',
+    ACTIVITIES: 'NO-TASK',
+    CONTENT: 'DOM Manipulation',
+    MONTH: 'october'
+  },
+  {
+    ID: 23,
+    TITLE: 'NO_CLASSS',
+    ACTIVITIES: 'NO_CLASSS',
+    CONTENT: 'NO_CLASSS',
+    MONTH: 'october'
+  },
+  {
+    ID: 24,
+    TITLE: 'NO_CLASSS',
+    ACTIVITIES: 'NO_CLASSS',
+    CONTENT: 'NO_CLASSS',
+    MONTH: 'october'
+  },
+  {
+    ID: 25,
+    TITLE: 'DOM',
+    ACTIVITIES: 'NO-TASK',
+    CONTENT: 'Document Vs Window',
+    MONTH: 'october'
+  },
+  {
+    ID: 26,
+    TITLE: 'DOM',
+    ACTIVITIES: 'pagination,calculator',
+    CONTENT: 'Recap',
+    MONTH: 'october'
+  },
+  {
+    ID: 27,
+    TITLE: 'DOM',
+    ACTIVITIES: 'NO-TASK',
+    CONTENT: 'DOM Manipulation',
+    MONTH: 'october'
+  }
+]
+[
+  {
+    ID: 28,
+    TITLE: 'ASYNC PROGRAMING',
+    ACTIVITIES: 'call back hell',
+    CONTENT: 'Callback',
+    MONTH: 'october'
+  },
+  {
+    ID: 29,
+    TITLE: 'ASYNC PROGRAMING',
+    ACTIVITIES: 'fetch API',
+    CONTENT: 'Promise',
+    MONTH: 'october'
+  }
+]
---
+++ 4/count.js
@@ -0,0 +1,290 @@
+day1> db.codekata.find({status:"solved"},{program:1,status:1,topic:1,problem:1})
+[
+  {
+    _id: ObjectId('658944dab665d480d33914de'),
+    problem: '1.1',
+    topic: 'input&output',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914e0'),
+    problem: '1.3',
+    topic: 'input&output',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914e1'),
+    problem: '1.4',
+    topic: 'input&output',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914e3'),
+    problem: '1.6',
+    topic: 'input&output',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914e4'),
+    problem: '1.7',
+    topic: 'input&output',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914e6'),
+    problem: '1.9',
+    topic: 'input&output',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914e7'),
+    problem: '2',
+    topic: 'array',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914e9'),
+    problem: '2.2',
+    topic: 'array',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914ea'),
+    problem: '2.3',
+    topic: 'array',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914ec'),
+    problem: '2.5',
+    topic: 'array',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914ed'),
+    problem: '2.6',
+    topic: 'array',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914ef'),
+    problem: '2.8',
+    topic: 'string',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914f0'),
+    problem: '2.9',
+    topic: 'string',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914f2'),
+    problem: '3.1',
+    topic: 'numberd',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914f3'),
+    problem: '3.2',
+    topic: 'numberd',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914f5'),
+    problem: '3.4',
+    topic: 'numberd',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914f6'),
+    problem: '3.5',
+    topic: 'numberd',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914f8'),
+    problem: '3.7',
+    topic: 'dynamic programming',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914f9'),
+    problem: '3.8',
+    topic: 'dynamic programming',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914fb'),
+    problem: '4',
+    topic: 'patterns',
+    program: 'codekata',
+    status: 'solved'
+  }
+]
+
+[
+  {
+    _id: ObjectId('658944dab665d480d33914fc'),
+    problem: '4.1',
+    topic: 'patterns',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914fe'),
+    problem: '4.3',
+    topic: 'patterns',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d33914ff'),
+    problem: '4.4',
+    topic: 'patterns',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d3391501'),
+    problem: '4.6',
+    topic: 'patterns',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d3391502'),
+    problem: '4.7',
+    topic: 'patterns',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d3391504'),
+    problem: '4.89999999999999',
+    topic: 'patterns',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d3391505'),
+    problem: '5',
+    topic: 'bitwise',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d3391507'),
+    problem: '5.19999999999999',
+    topic: 'bitwise',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d3391508'),
+    problem: '5.29999999999999',
+    topic: 'bitwise',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d339150a'),
+    problem: '5.49999999999999',
+    topic: 'bitwise',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d339150b'),
+    problem: '5.59999999999999',
+    topic: 'bitwise',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d339150d'),
+    problem: '5.79999999999999',
+    topic: 'bitwise',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d339150e'),
+    problem: '5.89999999999999',
+    topic: 'bitwise',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d3391510'),
+    problem: '6.09999999999999',
+    topic: 'stack',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d3391511'),
+    problem: '6.19999999999999',
+    topic: 'stack',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d3391513'),
+    problem: '6.39999999999999',
+    topic: 'stack',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d3391514'),
+    problem: '6.49999999999999',
+    topic: 'stack',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d3391516'),
+    problem: '6.69999999999999',
+    topic: 'stack',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d3391517'),
+    problem: '6.79999999999999',
+    topic: 'stack',
+    program: 'codekata',
+    status: 'solved'
+  },
+  {
+    _id: ObjectId('658944dab665d480d3391519'),
+    problem: '6.99999999999999',
+    topic: 'stack',
+    program: 'codekata',
+    status: 'solved'
+  }
+]
+// Type "it" for more
+// day1> db.codekata.find({status:"solved"},{program:1,status:1,topic:1,problem:1}).count("$status")
+// 66
+// day1>
